07e272f2cceddcf3a0e7d56752be7acee4184b4c
Valeriia Kachanova <kachanova.valeriia11@gmail.com>
2025-12-07 18:11:11 +0100

    Update homepage text and routing HW07

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function HomePage() {
         <h1 className={css.title}>Welcome to NoteHub</h1>
 
         <p className={css.description}>
-          NoteHub — простое и эффективное приложение для управления личными заметками. 
-          Оно помогает хранить ваши мысли организованно и в одном месте, где бы вы ни находились: дома или в дороге.
+          NoteHub is a simple and efficient application for managing personal notes. 
+          It helps you keep your thoughts organized and accessible in one place — whether youre at home or on the go.
         </p>
 
         <p className={css.description}>
-          Приложение предоставляет понятный интерфейс для написания, редактирования и просмотра заметок. 
-          Благодаря поддержке поиска по ключевым словам и структурированной организации, NoteHub предлагает 
-          удобный интерфейс для всех, кто ценит ясность и продуктивность.
+          The app provides an intuitive interface for creating, editing, and viewing notes. 
+          With features like keyword search and structured organization, NoteHub offers a clean and productive 
+          environment for everyone who values clarity and efficiency.
         </p>
       </div>
     </main>
